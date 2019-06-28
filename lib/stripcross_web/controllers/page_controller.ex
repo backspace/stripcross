6e00b09e6cd8f59e2076126d1a34a23af4f0d4d9
Buck Doyle <b@chromatin.ca>
2019-06-28 01:23:12 -0500

    Add preliminary styling

--- lib/stripcross_web/controllers/page_controller.ex
+++ lib/stripcross_web/controllers/page_controller.ex
@@ -13,8 +13,44 @@ defmodule StripcrossWeb.PageController do
     clues_selector = Application.get_env(:stripcross, :clues_selector)
     clues = ModestEx.find(body, clues_selector)
 
+    document = """
+    <html>
+      <head>
+        <style>
+          table {
+            border-collapse: collapse;
+          }
+
+          td {
+            width: 2rem;
+            height: 2rem;
+
+            border: 1px solid black;
+            vertical-align: top;
+
+            font-family: sans-serif;
+            font-size: 8px;
+          }
+
+          td.filled {
+            background: repeating-linear-gradient(
+              45deg,
+              white,
+              #888 2px,
+              white 2px,
+              #888 2px
+            );
+          }
+        </style>
+        #{title}
+      </head>
+      <body>
+      </body>
+    </html>
+    """
+
     transformed =
-      ModestEx.append("<html><head>#{title}</head><body></body></html>", "body", puzzle_table)
+      ModestEx.append(document, "body", puzzle_table)
       |> ModestEx.remove(".letter")
       |> ModestEx.append("body", clues)
       |> ModestEx.remove("#{clues_selector} a")
