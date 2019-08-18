c48d97f786409232bcf9fab21c1e074bb91e5a11
Buck Doyle <b@chromatin.ca>
2019-08-17 20:44:38 -0500

    Update router-matcher regex with more specificity
    Maybe it would be preferable to distinguish invalid dates
    from other things but… this’ll do 😬

--- test/stripcross_web/controllers/page_controller_test.exs
+++ test/stripcross_web/controllers/page_controller_test.exs
@@ -6,4 +6,10 @@ defmodule StripcrossWeb.PageControllerTest do
       get(conn, "/something-else")
     end
   end
+
+  test "GET /2019-19-01", %{conn: conn} do
+    assert_error_sent :not_found, fn ->
+      get(conn, "/2019-19-01")
+    end
+  end
 end
