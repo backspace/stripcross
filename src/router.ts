232121977886c271c54157ded74fe90bb9181642
Buck Doyle <b@chromatin.ca>
2022-09-24 12:41:35 -0500

    Remove health route

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,8 @@
 import Router from 'koa-router';
 import IndexRoute from './routes';
-import HealthRoutes from './routes/health';
 
 const router = new Router();
 
 IndexRoute.register(router);
-HealthRoutes.register(router);
 
 export { router };
