ab77219726c7b7bb8e2b189bd26ddec4c22244f4
Buck Doyle <b@chromatin.ca>
2022-09-24 12:39:45 -0500

    Add preliminary transformation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,11 @@
 import Router from 'koa-router';
+import { JSDOM } from 'jsdom';
 import 'cross-fetch';
+
+function extractSelector(document: Document, selector: string) {
+  const element = document.querySelector(selector);
+  return element?.outerHTML || '';
+}
 
 const register = (router: Router) => {
   router.get('/', async ctx => {
@@ -8,7 +14,24 @@ const register = (router: Router) => {
     const original = await fetch(`${process.env.BASE_HOST}`);
     const html = await original.text();
 
-    ctx.body = html;
+    const { document } = new JSDOM(html).window;
+
+    process.env.REMOVE_SELECTORS?.split(' ').forEach(selector => {
+      document.querySelectorAll(selector).forEach(element => element.remove());
+    });
+
+    const newDocumentString = `
+        <html>
+            <body>
+                ${extractSelector(document, 'h1:first-of-type')}
+                ${extractSelector(document, 'h1 + h2:first-of-type')}
+                ${extractSelector(document, process.env.PUZZLE_SELECTOR!)}
+                ${extractSelector(document, process.env.CLUES_SELECTOR!)}
+            </body>
+        </html>
+    `;
+
+    ctx.body = newDocumentString;
   });
 };
 
--- src/server.test.ts
+++ src/server.test.ts
@@ -1,6 +1,7 @@
 import fetchMock from 'jest-fetch-mock';
 import request from 'supertest';
 import type { Server } from 'http';
+import { JSDOM } from 'jsdom';
 import { createServer } from './server';
 
 fetchMock.enableMocks();
@@ -15,9 +16,48 @@ describe('stripcross', () => {
   });
 
   test('/', async () => {
-    fetchMock.mockResponse('a response');
+    fetchMock.mockResponse(`
+        <html>
+            <head>
+            <title>Hello</title>
+            </head>
+            <body>
+            <div id=ignored>this is ignored</div>
+            <h1 id=Title></h1>
+            <h2 id=Subtitle></h2>
+            <div id=Passthrough>something</div>
+            <table id=Puzzle>
+                <tr><td id=preserved>this is preserved</td></tr>
+                <tr>
+                <td class=letter>this is removed</td>
+                <td class="something"></td>
+                <td class="something-else"></td>
+                </tr>
+            </table>
+            <div id=Clues>
+                <div>1</div>
+                <div id=preservedclue>A clue : <a>AN ANSWER</a></div>
+            </div>
+            <div id=OtherPassthrough></div>
+            <div>
+                <h1 id=IgnoredTitle></h1>
+                <h2 id=IgnoredSubtitle></h2>
+            </div>
+            </body>
+        </html>
+    `);
 
     const response = await request(server).get('/');
-    expect(response.text).toEqual('a response');
+
+    const { document } = new JSDOM(response.text).window;
+
+    expect(document.querySelector('#Title')).not.toBeNull();
+    expect(document.querySelector('#Subtitle')).not.toBeNull();
+
+    expect(document.querySelector('#ignored')).toBeNull();
+    expect(document.querySelector('.letter')).toBeNull();
+
+    expect(document.querySelector('#preserved')).not.toBeNull();
+    expect(document.querySelector('#preservedclue')).not.toBeNull();
   });
 });
