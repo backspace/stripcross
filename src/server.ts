f52f29cee6a0de0be5fb937bd35bbce8d7fa1952
Buck Doyle <b@chromatin.ca>
2022-09-27 22:11:58 -0500

    Add request logging

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import cors from '@koa/cors';
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
+import logger from 'koa-logger';
 import { bodyParserConfig, corsConfig } from './config';
 import { router } from './router';
 
 export function createServer(): Koa {
   const app = new Koa();
+
+  app.use(logger());
 
   // Apply Middleware
   app.use(bodyParser(bodyParserConfig));
